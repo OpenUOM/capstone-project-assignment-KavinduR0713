b7bd3a6ba2da43afef4b3c06bf482fa756bb4d02
KavinnduR0713 <nirmalrathnasiri85@gmail.com>
2023-03-20 23:58:52 +0530

    Changes_new

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,5 @@
 const server  = require ("./server.js");
 
-import server from "./server.js";
-
-server.listen(8080, function () {
-  console.log(
-    "Capstone Project Backend is running on http://localhost:8080"
-  );
-});
 
 if(process.env.NODE_ENV === "test"){
   server.listen(3000, () => {
